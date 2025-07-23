01e4ed03c6b846b61b36f138b988dd705e8c0f6e
Eugene Molotov <molotov@it-projects.info>
2025-07-23 14:32:08 +0500

    [ADD] bacula2_contacts: добавил модификацию

---
+++ bacula2_contacts/README.rst
@@ -0,0 +1,5 @@
+===========================================
+ Contacts modifications for Bacula Systems
+===========================================
+
+* In contact form, default type for subcontact is 'contact' instead of 'other' (Other address)
---
+++ bacula2_contacts/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": """Contacts modifications for Bacula Systems""",
+    "version": "16.0.0.1.0",
+    "author": "IT-Projects LLC, Eugene Molotov",
+    "support": "it@it-projects.info",
+    "website": "https://github.com/it-projects-llc/bacula-addons",
+    "license": "LGPL-3",
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "demo": [],
+}
